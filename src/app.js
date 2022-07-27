ed96b35eeba15e3f044ef14caf0e0734b4305a19
jparrar <jparrar@unal.edu.co>
2022-07-27 01:23:53 -0500

    First light bulb (A)  working

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
-const lightOn = "/assets/img/lightOn.png";
-const lightOff = "/assets/img/lightOff.png";
+const lightOn = "/assets/img/lightOn.png", lightOff = "/assets/img/lightOff.png"; 
+const red = "#e93d3d", green = "#41d72f";
 
-function changeSwitch (buttonId) {
-	var switchId = document.getElementById(buttonId).innerHTML;
-	if (switchId == "On") {
+
+function switchAction (switchId, buttonId, lightId) {
+	if (switchId == 'On') {
 		document.getElementById(buttonId).innerHTML = "Off";
-		document.getElementById("light1").src = lightOn; // Not working cus the light number
+		document.getElementById(buttonId).style.backgroundColor = red;
+		document.getElementById(lightId).src = lightOff; 
 	}
 	else if (switchId == "Off") {
 		document.getElementById(buttonId).innerHTML = "On";
-		document.getElementById("light1").src = lightOff;
+		document.getElementById(buttonId).style.backgroundColor = green;
+		document.getElementById(lightId).src = lightOn;
 	}
+}
+ 
+function switchLightA (buttonId, lightId) {
+	var switchId = document.getElementById(buttonId).innerHTML;
+	switchAction(switchId, buttonId, lightId);
 }
 
